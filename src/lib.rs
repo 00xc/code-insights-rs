1a78ceb518dbb8584f10d0ff8b96c4d568957c5e
Fredrik Larsson <nossralf@gmail.com>
2020-02-29 00:19:45 +0100

    Add builder for Report
    Besides the expected setters add convenience methods to serialize the
    Report to a JSON `String` or a `serde_json::Value`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use serde_json::Number;
+use serde_json::{Number, Result, Value};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "UPPERCASE")]
@@ -31,6 +31,8 @@ pub enum Parameter<'a> {
     Text(&'a str),
 }
 
+/// A struct that represents a Bitbucket Server [Code Insights](https://confluence.atlassian.com/bitbucketserver/code-insights-966660485.html)
+/// report.
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Report<'a> {
@@ -65,6 +67,98 @@ pub struct Report<'a> {
     /// logo will be used.
     #[serde(skip_serializing_if = "Option::is_none")]
     logo_url: Option<&'a str>,
+}
+
+impl<'a> Report<'a> {
+    /// Constructs a new Code Insights `Report` with the title `title`.
+    ///
+    /// The maximum length of `title` is 450 characters. This is a Bitbucket
+    /// limitation. It is recommended to use a short title for display purposes
+    /// in Bitbucket.
+    pub fn new(title: &'a str) -> Report<'a> {
+        Report {
+            title,
+            details: None,
+            result: None,
+            data: None,
+            reporter: None,
+            link: None,
+            logo_url: None,
+        }
+    }
+
+    /// Sets the report's details.
+    ///
+    /// The report details are intended to describe the purpose of the report.
+    /// It may contain escaped newlines and if it does, Bitbucket will display
+    /// the content accordingly.
+    ///
+    /// The maximum length of `details` is 2000 characters. This is a Bitbucket
+    /// limitation.
+    pub fn details(&'a mut self, details: &'a str) -> &'a mut Report {
+        self.details = Some(details);
+        self
+    }
+
+    /// Sets the result of the `Report` which indicates whether the report is
+    /// in a passed or failed state.
+    pub fn result(&'a mut self, result: ReportResult) -> &'a mut Report {
+        self.result = Some(result);
+        self
+    }
+
+    /// Sets the data fields, which are used to display information related to
+    /// the report.
+    ///
+    /// Examples of data fields may be code coverage percentage or the number
+    /// of linter errors.
+    ///
+    /// A maximum of 6 `data` fields are allowed. This is a Bitbucket
+    /// limitation.
+    pub fn data(&'a mut self, data: Vec<Data<'a>>) -> &'a mut Report {
+        self.data = Some(data);
+        self
+    }
+
+    /// Sets the reporter.
+    ///
+    /// The reporter describes the tool or company which created the Code
+    /// Insights report.
+    ///
+    /// The maximum length of `reporter` is 450 characters. This is a Bitbucket
+    /// limitation.
+    pub fn reporter(&'a mut self, reporter: &'a str) -> &'a mut Report {
+        self.reporter = Some(reporter);
+        self
+    }
+
+    /// Set the report's link.
+    ///
+    /// The `link` is a URL linking to the results of the report in an external
+    /// tool.
+    pub fn link(&'a mut self, link: &'a str) -> &'a mut Report {
+        self.link = Some(link);
+        self
+    }
+
+    /// Set the report's logo URL.
+    ///
+    /// The report logo will be displayed by Bitbucket when the report is
+    /// presented to the user. It is recommended to use an SVG logo.
+    pub fn logo_url(&'a mut self, logo_url: &'a str) -> &'a mut Report {
+        self.logo_url = Some(logo_url);
+        self
+    }
+
+    /// Serialize the report to a JSON `String`.
+    pub fn to_string(&'a self) -> Result<String> {
+        serde_json::to_string(self)
+    }
+
+    /// Serialize the report to a `serde_json::Value`.
+    pub fn to_value(&'a self) -> Result<Value> {
+        serde_json::to_value(self)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
